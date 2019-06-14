fc9a906867f20c049698a7792ecede4809c1a43f
Natasha Waller <nnwaller@gmail.com>
2019-06-14 18:06:22 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,21 @@ class Show < ActiveRecord::Base
    def most_popular_show
      self.highest_rating
    end
+   
+   def lowest_rating
+     Show.minimum(:rating)
+   end
+   
+   def least_popular_show
+     self.lowest_rating
+   end
+   
+   def ratings_sum
+     Show.sum(:ratings)
+   end
+   
+   def popular_shows
+     Show.where("ratings > ?", 5)
+   end
+   
 end
