24f060b17c74ce0fee2bb9944b7b0b0249a01ee8
Natasha Waller <nnwaller@gmail.com>
2019-06-14 18:25:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,29 +1,30 @@
 class Show < ActiveRecord::Base
+
     def self.highest_rating
-      Show.maximum(:rating)
-   end 
-   
-   def self.most_popular_show
-     self.highest_rating
-   end
-   
-   def self.lowest_rating
-     Show.minimum(:rating)
-   end
-   
-   def self.least_popular_show
-     
-   end
-   
-   def self.ratings_sum
-     Show.sum(:ratings)
-   end
-   
-   def self.popular_shows
-     Show.where("ratings > ?", 5)
-   end
-   
-   def self.shows_by_alphabetical_order
-     Show.order(:name)
-   end
-end
+      self.maximum(:rating)
+    end
+  
+    def self.most_popular_show
+      self.where("rating = ?", self.highest_rating).first
+    end
+  
+    def self.lowest_rating
+      self.minimum(:rating)
+    end
+  
+    def self.least_popular_show
+      self.where("rating = ?", self.lowest_rating).first
+    end
+  
+    def self.ratings_sum
+      self.sum(:rating)
+    end
+    
+    def self.popular_shows
+      self.where("rating >= ?", 5)
+    end
+  
+    def self.shows_by_alphabetical_order
+      self.order(:name)
+    end
+  end
